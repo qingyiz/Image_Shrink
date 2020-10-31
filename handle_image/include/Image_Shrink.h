bc4518c68d4e5a3b3ccbaa46cc67eb13634c8d32
qingyiz <qingyizhu12138@163.com>
2020-10-31 11:26:34 +0800

    add crop_effect_area

--- handle_image/include/Image_Shrink.h
+++ handle_image/include/Image_Shrink.h
@@ -8,6 +8,14 @@
 using namespace cv;
 
 
+
+struct object_rect {
+    int x;      /*左上角x*/
+    int y;      /*左上角y*/
+    int width;  /*长度*/
+    int height; /*宽度*/
+};
+
 /*函数名：	resize_uniform()
  *函数功能：等比例缩放函数
  *参数：src 原始图像
@@ -15,10 +23,14 @@ using namespace cv;
  *		dst_size 目标图像的大小
  *		effect_area 有效区域，就是非黑色区域
  */
-struct object_rect {
-    int x;      /*左上角x*/
-    int y;      /*左上角y*/
-    int width;  /*长度*/
-    int height; /*宽度*/
-};
 int resize_uniform(Mat &src, Mat &dst, Size dst_size, object_rect &effect_area);
+
+
+/*函数名：	crop_effect_area()
+ *函数功能：等比例恢复函数
+ *参数：src 等比例缩放后的图像
+ *		dst 输出目标图像
+ *		dst_size 等比例缩放之前的原始图像大小
+ *		effect_area 等比例缩放后的有效区域
+ */
+int crop_effect_area(Mat &uniform_scaled, Mat &dst, Size ori_size, object_rect effect_area);
